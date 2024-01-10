c91ea5820515c6a71eb7c55eb5fd0a0018b167cc
dharmeshKochariOS <jineshsonawat9929@gmail.com>
2024-01-10 22:06:29 +0530

    files: added the section

--- src/App.js
+++ src/App.js
@@ -15,13 +15,19 @@ import tools from "./icons/Tools.png";
 import discounts from "./icons/Discounts.png";
 import wallet from "./icons/wallet.png";
 import questionmark from "./headers/Vector.png";
-
 import message from "./headers/Menu.png";
 import arrow from "./headers/arrow.png";
+import blackarrow from "./frames/arrow.png";
+import sort from "./frames/Sort.png";
+import download from "./frames/download.png";
+import orderArrow from "./frames/orderArrow.png";
+import transaction from "./frames/transaction.png";
+import rightArrow from "./frames/Chevron Right.png";
+import leftArrow from "./frames/Chevron left.png";
 
 function App() {
   return (
-    <div className="flex-container-row ">
+    <div className="flex-container-row whole">
       <nav className="navbar gap-row-24 flex-container-column">
         {/* For Company Icon */}
         <div className="flex-container margin-8 gap-column-12 height-42 ">
@@ -135,9 +141,258 @@ function App() {
           </div>
         </header>
 
-        <section className="Section">
-          <div></div>
-          <div></div>
+        <section className="section flex-container-column gap-row-32">
+          <div className="gap-row-24 flex-container-column">
+            <div className="flex-container">
+              <div>Overview</div>
+              <div className="flex-container gap-column-12 filter">
+                <div>Last Month</div>
+                <img src={blackarrow} alt="icon" />
+              </div>
+            </div>
+            <div className="flex-container-row gap-column-20">
+              <div className="databox flex-container-column gap-row-24">
+                <div style={{ fontSize: "12px", fontWeight: "300px" }}>
+                  Online Orders
+                </div>
+                <div style={{ fontSize: "24px" }}>231</div>
+              </div>
+              <div className="databox flex-container-column gap-row-24">
+                <div style={{ fontSize: "12px", fontWeight: "300px" }}>
+                  Amount Received
+                </div>
+                <div style={{ fontSize: "24px" }}>23,92,312.19</div>
+              </div>
+            </div>
+          </div>
+
+          <div className="flex-container-column gap-row-20">
+            <div>Transaction | This Month</div>
+            <div className="section-data ">
+              <div className="gap-row-12 flex-container-column">
+                <div className="flex-container">
+                  <div>
+                    <input
+                      className="searchBar-1"
+                      placeholder="Search by order ID..."
+                    />
+                  </div>
+                  <div className="flex-container gap-column-12">
+                    <div className="flex-container gap-column-6 sort">
+                      <div>Sort</div>
+                      <img src={sort} alt="icon" />
+                    </div>
+                    <div className="download ">
+                      <img src={download} alt="icon" />
+                    </div>
+                  </div>
+                </div>
+
+                <div className="flex-container gap-column-40 data-column-header">
+                  <div className="data-container">Order ID</div>
+                  <div className="flex-container-start gap-column-4 data-container">
+                    <div>Order date</div>
+                    <img
+                      style={{ width: "8px", height: "8px" }}
+                      src={orderArrow}
+                      alt="icon"
+                    />
+                  </div>
+                  <div className="data-container flex-container-1">
+                    Order Amount
+                  </div>
+                  <div className="flex-container-1 gap-column-4 data-container">
+                    <div>Transaction Fees</div>
+                    <img
+                      style={{ width: "14px", height: "14px" }}
+                      src={transaction}
+                      alt="icon"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div>
+                <div className="flex-container gap-column-40 data-column">
+                  <div className="data-container">#281209</div>
+                  <div className="  data-container">7 July, 2023</div>
+                  <div className="data-container flex-container-1">
+                    ₹1,278.23
+                  </div>
+                  <div className="flex-container-1  data-container">₹22</div>
+                </div>
+                <div className="flex-container gap-column-40 data-column">
+                  <div className="data-container">#281209</div>
+                  <div className="  data-container">7 July, 2023</div>
+                  <div className="data-container flex-container-1">
+                    ₹1,278.23
+                  </div>
+                  <div className="flex-container-1  data-container">₹22</div>
+                </div>
+                <div className="flex-container gap-column-40 data-column">
+                  <div className="data-container">#281209</div>
+                  <div className="  data-container">7 July, 2023</div>
+                  <div className="data-container flex-container-1">
+                    ₹1,278.23
+                  </div>
+                  <div className="flex-container-1  data-container">₹22</div>
+                </div>
+                <div className="flex-container gap-column-40 data-column">
+                  <div className="data-container">#281209</div>
+                  <div className="  data-container">7 July, 2023</div>
+                  <div className="data-container flex-container-1">
+                    ₹1,278.23
+                  </div>
+                  <div className="flex-container-1  data-container">₹22</div>
+                </div>
+                <div className="flex-container gap-column-40 data-column">
+                  <div className="data-container">#281209</div>
+                  <div className="  data-container">7 July, 2023</div>
+                  <div className="data-container flex-container-1">
+                    ₹1,278.23
+                  </div>
+                  <div className="flex-container-1  data-container">₹22</div>
+                </div>
+                <div className="flex-container gap-column-40 data-column">
+                  <div className="data-container">#281209</div>
+                  <div className="  data-container">7 July, 2023</div>
+                  <div className="data-container flex-container-1">
+                    ₹1,278.23
+                  </div>
+                  <div className="flex-container-1  data-container">₹22</div>
+                </div>
+                <div className="flex-container gap-column-40 data-column">
+                  <div className="data-container">#281209</div>
+                  <div className="  data-container">7 July, 2023</div>
+                  <div className="data-container flex-container-1">
+                    ₹1,278.23
+                  </div>
+                  <div className="flex-container-1  data-container">₹22</div>
+                </div>
+                <div className="flex-container gap-column-40 data-column">
+                  <div className="data-container">#281209</div>
+                  <div className="  data-container">7 July, 2023</div>
+                  <div className="data-container flex-container-1">
+                    ₹1,278.23
+                  </div>
+                  <div className="flex-container-1  data-container">₹22</div>
+                </div>
+                <div className="flex-container gap-column-40 data-column">
+                  <div className="data-container">#281209</div>
+                  <div className="  data-container">7 July, 2023</div>
+                  <div className="data-container flex-container-1">
+                    ₹1,278.23
+                  </div>
+                  <div className="flex-container-1  data-container">₹22</div>
+                </div>
+                <div className="flex-container gap-column-40 data-column">
+                  <div className="data-container">#281209</div>
+                  <div className="  data-container">7 July, 2023</div>
+                  <div className="data-container flex-container-1">
+                    ₹1,278.23
+                  </div>
+                  <div className="flex-container-1  data-container">₹22</div>
+                </div>
+                <div className="flex-container gap-column-40 data-column">
+                  <div className="data-container">#281209</div>
+                  <div className="  data-container">7 July, 2023</div>
+                  <div className="data-container flex-container-1">
+                    ₹1,278.23
+                  </div>
+                  <div className="flex-container-1  data-container">₹22</div>
+                </div>
+                <div className="flex-container gap-column-40 data-column">
+                  <div className="data-container">#281209</div>
+                  <div className="  data-container">7 July, 2023</div>
+                  <div className="data-container flex-container-1">
+                    ₹1,278.23
+                  </div>
+                  <div className="flex-container-1  data-container">₹22</div>
+                </div>
+                <div className="flex-container gap-column-40 data-column">
+                  <div className="data-container">#281209</div>
+                  <div className="  data-container">7 July, 2023</div>
+                  <div className="data-container flex-container-1">
+                    ₹1,278.23
+                  </div>
+                  <div className="flex-container-1  data-container">₹22</div>
+                </div>
+                <div className="flex-container gap-column-40 data-column">
+                  <div className="data-container">#281209</div>
+                  <div className="  data-container">7 July, 2023</div>
+                  <div className="data-container flex-container-1">
+                    ₹1,278.23
+                  </div>
+                  <div className="flex-container-1  data-container">₹22</div>
+                </div>
+                <div className="flex-container gap-column-40 data-column">
+                  <div className="data-container">#281209</div>
+                  <div className="  data-container">7 July, 2023</div>
+                  <div className="data-container flex-container-1">
+                    ₹1,278.23
+                  </div>
+                  <div className="flex-container-1  data-container">₹22</div>
+                </div>
+                <div className="flex-container gap-column-40 data-column">
+                  <div className="data-container">#281209</div>
+                  <div className="  data-container">7 July, 2023</div>
+                  <div className="data-container flex-container-1">
+                    ₹1,278.23
+                  </div>
+                  <div className="flex-container-1  data-container">₹22</div>
+                </div>
+                <div className="flex-container gap-column-40 data-column">
+                  <div className="data-container">#281209</div>
+                  <div className="  data-container">7 July, 2023</div>
+                  <div className="data-container flex-container-1">
+                    ₹1,278.23
+                  </div>
+                  <div className="flex-container-1  data-container">₹22</div>
+                </div>
+                <div className="flex-container gap-column-40 data-column">
+                  <div className="data-container">#281209</div>
+                  <div className="  data-container">7 July, 2023</div>
+                  <div className="data-container flex-container-1">
+                    ₹1,278.23
+                  </div>
+                  <div className="flex-container-1  data-container">₹22</div>
+                </div>
+              </div>
+
+              <div className="flex-container-column gap-row-24">
+                <div className="flex-container gap-column-40 data-column">
+                  <div className="data-container">#281209</div>
+                  <div className="  data-container">7 July, 2023</div>
+                  <div className="data-container flex-container-1">
+                    ₹1,278.23
+                  </div>
+                  <div className="flex-container-1  data-container">₹22</div>
+                </div>
+                <div className="flex-container-center gap-column-24">
+                  <div className="flex-container gap-column-6 previous">
+                    <img src={leftArrow} alt="icon" />
+                    <div>Previous</div>
+                  </div>
+                  <div className="flex-container-row gap-column-8 ">
+                    <div className="pagenation">1</div>
+                    <div className="pagenation">...</div>
+                    <div className="pagenation active">10</div>
+                    <div className="pagenation">11</div>
+                    <div className="pagenation">12</div>
+                    <div className="pagenation">13</div>
+                    <div className="pagenation">14</div>
+                    <div className="pagenation">15</div>
+                    <div className="pagenation">16</div>
+                    <div className="pagenation">17</div>
+                    <div className="pagenation">18</div>
+                  </div>
+                  <div className="flex-container gap-column-6 next">
+                    <div>Next</div>
+                    <img src={rightArrow} alt="icon" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </section>
       </div>
     </div>
